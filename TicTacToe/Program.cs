9ea6455308cdf7996f92d319641b83e35d195dbf
AlexandrSawrasov <caiiiuk@mail.ru>
2022-10-25 00:48:50 +0300

    alfa test

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -1,19 +1,14 @@
-﻿// Два игрока по очереди ставят крестики или нолики
-// на игровом поле, нажимая клавиши цифровой клавиатуры,
-// соответствующие ячейкам игрового поля. 
-// Выигрывает тот кто первым заполнит своими знаками
-// вертикальную, горизонтальную или диагональную линию.
+﻿//
+//
+//
+//
+//
 
-//          _________________ 
-//         |     |     |     |
-//         |  7  |  8  |  9  |
-//         |_____|_____|_____|
-//         |     |     |     |
-//         |  4  |  5  |  6  |
-//         |_____|_____|_____|
-//         |     |     |     |
-//         |  1  |  2  |  3  |
-//         |_____|_____|_____|
+//
+//
+//
+//
+
 
 
 
@@ -21,7 +16,8 @@
 void PrintGameBoard(char[] cage)
 {
     Console.Clear();
-    string[] gamecage = {  " _________________ ", 
+    string tab = "                                                    ";
+    string[] gamecage = {   " _________________ ", 
                             "|     |     |     |",
                 $"|  {cage[7]}  |  {cage[8]}  |  {cage[9]}  |",
                             "|_____|_____|_____|",
@@ -31,9 +27,10 @@ void PrintGameBoard(char[] cage)
                             "|     |     |     |",
                 $"|  {cage[1]}  |  {cage[2]}  |  {cage[3]}  |",
                             "|_____|_____|_____|"};
+    Console.WriteLine();
     for (int i = 0; i < 10; i++)
     {
-      Console.WriteLine(gamecage[i]); 
+      Console.WriteLine(tab + gamecage[i]); 
     }
 }
 
@@ -69,20 +66,54 @@ void KeyPressed(ConsoleKeyInfo key, char[] cage, char mark)
             if (cage[9] == ' ') cage[9] = mark;
             break;
     }
-
 }
+
                   //0   1   2   3   4   5   6   7   8   9  
 char[] gamecage = {' ',' ',' ',' ',' ',' ',' ',' ',' ',' '};
-   
-// bool win = (gamecage[7] !=' ' && gamecage[7] == gamecage[8] && gamecage[7] == gamecage[9]) || ;
-
-
+string[] welcome = {"                                  КРЕСТИКИ - НОЛИКИ            _________________",
+                    "          Два игрока по очереди ставят крестики или нолики    |     |     |     |",
+                    "     на игровом поле, нажимая клавиши цифровой клавиатуры,    |  7  |  8  |  9  |",
+                    "     соответствующие ячейкам игрового поля.                   |_____|_____|_____|",
+                    "          Выигрывает тот кто первым заполнит своими знаками   |     |     |     |",
+                    "     вертикальную, горизонтальную или диагональную линию.     |  4  |  5  |  6  |",
+                    "          Так что определяемся, кто ходит первым - тянем      |_____|_____|_____|",
+                    "     спичку, бросаем монетку и т.д. И в бой!!                 |     |     |     |",
+                    "          Первыми ходят крестики. Для начала игры нажмите     |  1  |  2  |  3  |",
+                    "     любую клавишу.                                           |_____|_____|_____|"};
+bool win = false;
+char tictac = 'X';
+int turn = 0;
+bool wturn = true;
+string tab = "                                                    ";
+Console.WriteLine();
+for (int i = 0; i < 10; i++)
+    {
+      Console.WriteLine(welcome[i]); 
+    }
+Console.ReadKey();
 PrintGameBoard(gamecage);
-KeyPressed(Console.ReadKey(), gamecage, 'X');
-
-
-
-
-// ConsoleKeyInfo key = Console.ReadKey();
-// if (key.Key == ConsoleKey.D5) Console.WriteLine("5");
-// Console.WriteLine("Go!!");
+while (!win && turn != 9)
+{
+    string boardA = new string(gamecage);
+    Console.WriteLine();
+    Console.WriteLine(tab + (wturn ? "  Ходят Крестики!" : "   Ходят Нолики!"));
+    KeyPressed(Console.ReadKey(), gamecage, tictac);
+    PrintGameBoard(gamecage);
+    win =  (gamecage[7] !=' ' && gamecage[7] == gamecage[8] && gamecage[7] == gamecage[9] ||
+            gamecage[4] !=' ' && gamecage[4] == gamecage[5] && gamecage[4] == gamecage[6] ||
+            gamecage[1] !=' ' && gamecage[1] == gamecage[2] && gamecage[1] == gamecage[3] ||
+            gamecage[7] !=' ' && gamecage[7] == gamecage[4] && gamecage[7] == gamecage[1] ||
+            gamecage[8] !=' ' && gamecage[8] == gamecage[5] && gamecage[8] == gamecage[2] ||
+            gamecage[9] !=' ' && gamecage[9] == gamecage[6] && gamecage[9] == gamecage[3] ||
+            gamecage[7] !=' ' && gamecage[7] == gamecage[5] && gamecage[7] == gamecage[3] ||
+            gamecage[1] !=' ' && gamecage[1] == gamecage[5] && gamecage[1] == gamecage[9]);
+    string boardZ = new string(gamecage);
+    turn = (boardA == boardZ ? turn : ++turn);
+    wturn = turn % 2 == 0;
+    tictac = (wturn ? 'X' : 'O');
+};
+Console.WriteLine();
+Console.Write(tab);
+if (win && turn <= 9) Console.WriteLine(wturn ? " Нолики победили!!" : "Крестики победили!!");
+else Console.WriteLine("   Боевая ничья!");
+Console.WriteLine();
